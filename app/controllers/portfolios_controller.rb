b3f6334112d122da8b9e0cab763bee5063f261c4
Tashby <tashby2714@gmail.com>
2017-05-06 16:48:12 -0400

    integrated index action for portfolio items

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,2 +1,26 @@
 class PortfoliosController < ApplicationController
+
+
+  def index
+    @portfolio_items = Portfolio.all
+  end
+
+  #def show
+
+  #end
+
+  #def view
+
+  #end
+
+  #def edit
+
+#  end
+
+#  def update
+
+##  def destroy
+
+#  end
+
 end
